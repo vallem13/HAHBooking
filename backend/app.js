66c51f0038af5dc25b24247e210a5f336c6b018b
Alexandra <ale.vallem@gmail.com>
2023-06-07 17:14:05 -0400

    Completed PHASE 2

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,41 @@ app.use(
 const routes = require('./routes');
 app.use(routes);
 
+const { ValidationError } = require('sequelize');
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
+
 
 
 module.exports = app;
