02657764a0ff3902ad4354f7d5abc6457efa0dfc
Alexandra <ale.vallem@gmail.com>
2023-06-12 18:57:19 -0400

    Completed Tables(Models and Migrations)

---
+++ backend/db/migrations/20230612194532-create-review-image.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ReviewImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "ReviewImages";
+    await queryInterface.dropTable(options);
+  }
+};
