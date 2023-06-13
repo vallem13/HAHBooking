361d40412b5c9e690b5b994b00f462acbd53233d
Alexandra <ale.vallem@gmail.com>
2023-06-13 19:12:11 -0400

    Completed POST /spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,6 +6,7 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
+// 1. Get all spots w/ avg rating and images
 router.get('/', async (req, res, next) => {
     const spots = await Spot.findAll({
         include: [
@@ -45,6 +46,53 @@ router.get('/', async (req, res, next) => {
     })
 })
 
+// 4. Post a new spot
+const createSpotChecker = (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+
+    const errors = {}
+
+    if (!address) errors.address = 'Street address is required'
+    if (!city) errors.city = 'City is required'
+    if (!state) errors.state = 'State is required'
+    if (!country) errors.country = 'Country is required'
+    if (!lat) errors.lat = 'Latitude is not valid'
+    if (!lng) errors.lng = 'Longitude is not valid'
+    if (!name) errors.name = 'Name must be less than 50 characters'
+    if (!description) errors.description = 'Description is required'
+    if (!price) errors.price = 'Price per day is required'
+
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json({
+            message: 'Bad Request',
+            errors: errors
+        })
+    }
+    next()
+}
+
+
+router.post('/', requireAuth, createSpotChecker, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    })
+
+    res.status(201)
+    res.json(newSpot)
+})
+
+// 2. Get all spots by the current user that is logged in w/ avg rating and images
 router.get('/current', requireAuth, async (req, res, next) => {
 
     const spots = await Spot.findAll({
@@ -88,6 +136,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
     })
 })
 
+// 3. Get spot by spotId w/  avg rating, images, and owner
 router.get('/:spotId', async (req, res, next) => {
     const spot = await Spot.findByPk(req.params.spotId, {
         include: [
