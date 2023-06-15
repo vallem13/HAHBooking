02dfffbe600f44f2457b0014884e6eb85d8ed792
Alexandra <ale.vallem@gmail.com>
2023-06-15 14:21:15 -0400

    Completed DELETE /:imageId

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -7,6 +7,31 @@ const { Op } = require('sequelize')
 
 const router = express.Router();
 
+// 19. Delete a review by imageId
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const reviewImage = await Review.findOne({
+        where: {
+            id: req.params.imageId
+        },
+        include: {
+            model: ReviewImage
+        }
+    })
 
+    if (!reviewImage) {
+        return res.status(404).json({ message: "Spot couldn't be found"})
+    }
+
+    if (reviewImage.userId !== req.user.id) {
+        return res.status(403).json({ message: "Forbidden"})
+    }
+
+    await reviewImage.destroy()
+
+    res.status(200)
+    res.json({
+        message: 'Successfully deleted'
+    })
+})
 
 module.exports = router;
