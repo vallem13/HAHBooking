4ca9bed4c8cd02e52831c698cfe7ea9a6de11f44
Alexandra <ale.vallem@gmail.com>
2023-06-15 12:16:13 -0400

    Completed PUT /:bookingId

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -3,7 +3,7 @@ const { Booking, User, Spot, SpotImage } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-
+const { Op } = require('sequelize')
 
 const router = express.Router();
 
@@ -44,7 +44,86 @@ router.get('/current', requireAuth, async (req, res, next) => {
     })
 })
 
-// 17.
+// 17. Edit all bookings by spotId
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+
+    const booking = await Booking.findByPk(req.params.bookingId)
+
+    if (!booking) {
+        return res.status(404).json({ message: "Booking couldn't be found"})
+    }
+
+    if (booking.userId !== req.user.id) {
+        return res.status(403).json({ message: "Forbidden"})
+    }
+
+    const { startDate, endDate } = req.body
+
+    const checkAvailability = await Booking.findOne({
+        where: {
+            spotId: req.params.bookingId,
+            [Op.or]: [
+                {
+                    startDate: {[Op.between]: [startDate, endDate]}
+                },
+                {
+                    endDate: {[Op.between]: [startDate, endDate]}
+                },
+            ]
+        }
+    })
+
+    if (checkAvailability) {
+        return res.status(403).json({
+            message: "Sorry, this spot is already booked for the specified dates",
+            errors: {
+              startDate: "Start date conflicts with an existing booking",
+              endDate: "End date conflicts with an existing booking"
+            }
+        })
+    }
+
+    let currentDate = new Date().toJSON().slice(0, 10);
+
+    if(startDate < currentDate) {
+        return res.status(403).json({ message: "Past bookings can't be modified"})
+    }
+
+    let setObj = {}
+
+    if (startDate) {
+        setObj.startDate = startDate
+    }
+    if (endDate) {
+        setObj.endDate = endDate
+    }
+
+    booking.set(setObj)
+    await booking.save()
+
+    res.json(booking)
+
+
+})
+
+// 17. Delete a booking by bookingId
+router.delete('/:bookingId', requireAuth, async (req, res, next) => {
+    const booking = await Booking.findByPk(req.params.reviewId)
+
+    if (!booking) {
+        return res.status(404).json({ message: "Booking couldn't be found"})
+    }
+
+    if (booking.userId !== req.user.id) {
+        return res.status(403).json({ message: "Forbidden"})
+    }
+
+    await booking.destroy()
+
+    res.json({
+        message: 'Successfully deleted'
+    })
+})
 
 
 
