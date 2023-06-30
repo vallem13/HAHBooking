6c1a89700edb5629297c9b9950bf3ce57177137c
Alexandra <ale.vallem@gmail.com>
2023-06-30 00:04:28 -0400

    completed phase 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -14,8 +15,11 @@ function App() {
   return (
     isLoaded && (
       <Switch>
-        <Route path="/login">
+        <Route exact path="/login">
           <LoginFormPage />
+        </Route>
+        <Route exact path="/signup">
+          <SignupFormPage />
         </Route>
       </Switch>
     )
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -47,6 +47,23 @@ export const restoreUser = () => async (dispatch) => {
     return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 
 const initialState = { user: null };
 
