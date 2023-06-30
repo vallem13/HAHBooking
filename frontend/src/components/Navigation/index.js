66378ab2490c2600f03712e8d38761ed6794d738
Alexandra <ale.vallem@gmail.com>
2023-06-30 17:39:46 -0400

    Logo onClick to home page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const history = useHistory()
+
+  const handleLogoClick = () => {
+    history.push('/')
+  }
 
   return (
     <nav id='Navigation-Bar'>
-      <img className='logo' src='../logo/hahbooking-high-resolution-color-logo.png' alt='HAHBooking'/>
+      <img className='logo' src='../logo/hahbooking-high-resolution-color-logo.png' alt='HAHBooking' onClick={handleLogoClick} />
       <ul>
         <li>
           <NavLink exact to="/">Home</NavLink>
